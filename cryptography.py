fc49fca9804c1371d3823d7dc2199299690fb142
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-10-09 13:04:51 -0400

    Updated from Brython Server: 10/9/2018 1:04:52 PM

--- cryptography.py
+++ cryptography.py
@@ -1,12 +1,52 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Eric Goodney
+Credit: Peers, Teacher, Internet:
 
 Assignment:
 
 Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
+
+Enter e to encrypt, d to decrypt, or q to quit: z
+Did not understand command, try again.
+Enter e to encrypt, d to decrypt, or q to quit: e
+Message: Two plus two = Five
+Key: Lorem ipsum
++KF;B(CH=NIZ}m;R\Dt
+Enter e to encrypt, d to decrypt, or q to quit: d
+Message: +KF;B(CH=NIZ}m;R\Dt
+Key: Lorem ipsum
+Two plus two = Five
+Enter e to encrypt, d to decrypt, or q to quit: q
+Goodbye!
+
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+z = "Did not understand command, try again."
+e = "Message: Two plus two = Five"
+d = "Message: +KF;B(CH=NIZ}m;R\Dt"
+q = "Goodbye!"
+
+
+option = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+if option == z:
+
+option = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+option = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+option = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+
+
+
+
+
+
+
+
+
+
