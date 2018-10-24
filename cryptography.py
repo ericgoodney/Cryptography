b53799482a2462dbfbe44e3303968d926a913f9c
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-10-24 12:27:55 -0400

    Updated from Brython Server: 10/24/2018 12:27:55 PM

--- cryptography.py
+++ cryptography.py
@@ -29,9 +29,7 @@ action = input("Enter e to encrypt, d to decrypt, or q to quit: ")
 
 
 while action !="q":
-    if action !="e" and action!="d" and action !="q":
-        print("Did not understand command, try again.")
-        action = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    
     if action == "e" or action =="d":
         m = input("Message: ")
         k = input("Key: ")
