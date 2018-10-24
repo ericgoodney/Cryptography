a98f5a45b31e5a4cddea9b2b97474bbc53de972d
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-10-24 12:11:04 -0400

    Updated from Brython Server: 10/24/2018 12:11:03 PM

--- cryptography.py
+++ cryptography.py
@@ -32,41 +32,41 @@ if action !="e" and action!="d" and action !="q":
     action = input("Enter e to encrypt, d to decrypt, or q to quit: ")
 
 while action !="q":
-    if action == "e" or "d":
+    if action == "e" or action =="d":
         m = input("Message: ")
         k = input("Key: ")
-    message = []
-    for l in m:
-        c = associations.find(l)
-        message.append(c)
-    mkey = []
-    for l in k:
-        f = associations.find(l)
-        mkey.append(f)
-        
-    L1 = len(message)
-    L2 = len(mkey)
-    rkey = mkey*int((L1/L2))
-    remainders = L1%L2 
-    if remainders !=0:
-        remainder = range(remainders)
-        for k in remainder:
-            rkey.append(mkey[k])
-    result = []
-    elist = (list(zip(message,rkey)))
-    if action == "e":
-        for c in elist:
-            g = associations[c[0]+c[1]]
-            print(g,end="")
-    if action == "d":
-        for c in elist:
-            gg = associations[c[0]-c[1]]
-            print(gg,end="")
-    for l in result:
-        print(l,end="")
+        message = []
+        for l in m:
+            c = associations.find(l)
+            message.append(c)
+        mkey = []
+        for l in k:
+            f = associations.find(l)
+            mkey.append(f)
+            
+        L1 = len(message)
+        L2 = len(mkey)
+        rkey = mkey*int((L1/L2))
+        remainders = L1%L2 
+        if remainders !=0:
+            remainder = range(remainders)
+            for k in remainder:
+                rkey.append(mkey[k])
+        result = []
+        elist = (list(zip(message,rkey)))
+        if action == "e":
+            for c in elist:
+                g = associations[c[0]+c[1]]
+                print(g,end="")
+        if action == "d":
+            for c in elist:
+                gg = associations[c[0]-c[1]]
+                print(gg,end="")
+        for l in result:
+            print(l,end="")
     if action !="e" and action !="d" and action !="q":
-        print("Did not understnad command, try again")
-    print(' ')
+        print("Did not understand command, try again")
+    
     action = input("Enter e to encrypt, d to decrypt, or q to quit: ")
     
 if action == "q":
